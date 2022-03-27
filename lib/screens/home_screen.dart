6ee3926351acd03d50ebc9b689bb37cfe84fcfe2
Luiss Montoya <luisgoemon@gmail.com>
2022-03-27 17:13:31 -0500

    movieSlider_end

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,7 +22,7 @@ class HomeScreen extends StatelessWidget {
               CardSwiper(),
 
               //Slider de peliculas
-              MovieSlider()
+              MovieSlider(),
             ],
           ),
         ));
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -5,8 +5,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 260,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -17,6 +16,7 @@ class MovieSlider extends StatelessWidget {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
+          SizedBox(height: 5),
           Expanded(
             child: ListView.builder(
                 scrollDirection: Axis.horizontal,
@@ -30,15 +30,35 @@ class MovieSlider extends StatelessWidget {
 }
 
 class _MoviePoster extends StatelessWidget {
-  const _MoviePoster({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 130,
-      height: 190,
-      color: Colors.green,
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-    );
+        width: 130,
+        height: 190,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.pushNamed(context, 'details',
+                  arguments: 'movie-instance'),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Text(
+              'SpiderMan: De vuelta a casa5465456456465465456456456',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            )
+          ],
+        ));
   }
 }
