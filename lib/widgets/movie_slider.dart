cc4160bb26d93412f7082e3fc21f9edf418153d8
Luiss Montoya <luisgoemon@gmail.com>
2022-04-15 19:40:55 -0500

    animated_hero

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -60,8 +60,9 @@ class _MovieSliderState extends State<MovieSlider> {
                 controller: scrollController,
                 scrollDirection: Axis.horizontal,
                 itemCount: widget.movies.length,
-                itemBuilder: (_, int index) =>
-                    _MoviePoster(widget.movies[index])),
+                itemBuilder: (_, int index) => _MoviePoster(
+                    widget.movies[index],
+                    '${widget.title}-$index-${widget.movies[index].id}')),
           )
         ],
       ),
@@ -72,11 +73,13 @@ class _MovieSliderState extends State<MovieSlider> {
 class _MoviePoster extends StatelessWidget {
   //recibir final Movie movie:
   final Movie movie;
+  final String heroId;
 
-  const _MoviePoster(this.movie);
+  const _MoviePoster(this.movie, this.heroId);
 
   @override
   Widget build(BuildContext context) {
+    movie.heroId = heroId;
     return Container(
         width: 130,
         height: 190,
@@ -86,14 +89,17 @@ class _MoviePoster extends StatelessWidget {
             GestureDetector(
               onTap: () =>
                   Navigator.pushNamed(context, 'details', arguments: movie),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: FadeInImage(
-                  placeholder: AssetImage('assets/no-image.jpg'),
-                  image: NetworkImage(movie.fullPosterImg),
-                  width: 130,
-                  height: 190,
-                  fit: BoxFit.cover,
+              child: Hero(
+                tag: movie.heroId!,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: FadeInImage(
+                    placeholder: AssetImage('assets/no-image.jpg'),
+                    image: NetworkImage(movie.fullPosterImg),
+                    width: 130,
+                    height: 190,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
